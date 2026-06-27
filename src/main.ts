8473f2a32871cf16be024784315d8680f32c37cd
Gaziav <bebraziana@gmail.com>
2026-06-27 14:57:03 +0300

    1) Cоздал сервер.
    2) Добавил маршрут для метода GET.
    
    3) Добавил обработчки для методов POST, PUT, DELETE.
    
    4) Отправил(проверил) запросы через Postman.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import express from 'express';
 
 const server = express();
-
 const port = 2000;
 
-server.listen(port, () => {
-    console.log(`Server is started on port ${port}...`);
+server.get('/', (req, res) => {
+  console.log('Пришел запрос с методом GET');
+  res.send('Hello World! GET');
 });
+
+
+server.post('/', (req, res) => {
+  console.log('Пришел запрос с методом POST');
+  res.send('Hello World! POST');
+});
+
+server.put('/', (req, res) => {
+  console.log('Пришелл запрос с методом PUT');
+  res.send('Hello World! PUT');
+});
+
+server.delete('/', (req, res) => {
+  console.log('Пришел запрос с методом DELETE');
+  res.send('Hello World! DELETE');
+});
+
+server.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);});
