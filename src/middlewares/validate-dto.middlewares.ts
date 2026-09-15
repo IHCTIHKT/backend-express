bb9e70284a0a34af5d1d8f77d5a772632fb7bee7
CodeWalker <bebraziana@gmail.com>
2026-09-15 17:15:23 +0300

    Доработанный ДТО ☠️☠️☠️

--- src/middlewares/validate-dto.middlewares.ts
+++ src/middlewares/validate-dto.middlewares.ts
@@ -4,4 +4,4 @@ import { Request, Response, NextFunction } from 'express';
 
 export const validateDTO = (req: Request, res: Response, next: NextFunction) => {
 
-}
+};
--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -1,16 +1,30 @@
 import express from 'express';
 import { getMockUser } from '../../mocks/get-user-mocks';
+import { plainToInstance } from 'class-transformer';
+import { validateSync } from 'class-validator';
 import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
 const userRouter = express.Router();
 
 userRouter.post('/login', (req, res) => {
+  const dto = plainToInstance(UserLoginDto, req.body);
+  const error = validateSync(dto);
+  if (error.length > 0) {
+    res.status(400).json(error);
+    return;
+  }
   const user = getMockUser();
   res.json(user);
 });
 
 userRouter.post('/register', (req, res) => {
+  const dto = plainToInstance(UserRegisterDto, req.body);
+  const error = validateSync(dto);
+  if (error.length > 0) {
+    res.status(400).json(error);
+    return;
+  }
   const user = getMockUser();
-
   res.json(user);
 });
 
